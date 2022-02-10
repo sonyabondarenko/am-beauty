a406687baf62d949cb1686742813dd7e7969160a
sonyabondarenko <49877295+sonyabondarenko@users.noreply.github.com>
2022-02-10 19:13:20 +0100

    Add files via upload

--- common.js
+++ common.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function ($) {
   var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+        spaceBetween: 0,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+          },
+        
+        },
         navigation: {
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
